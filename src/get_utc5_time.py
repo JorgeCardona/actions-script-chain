f5317fab2221fdff5e5c5a6ebde5c8610ee97a35
JorgeCardona <jorgecardona@utp.edu.co>
2024-10-13 01:24:11 +0000

    print comment

--- src/get_utc5_time.py
+++ src/get_utc5_time.py
@@ -4,9 +4,10 @@ def get_utc5_time():
     # Get the current time in UTC-5
     utc_offset = timedelta(hours=-5)
     current_time = datetime.utcnow() + utc_offset
-    return current_time.strftime("%Y-%m-%d %H:%M:%S")  # Time format
+    final_time = current_time.strftime("%Y-%m-%d %H:%M:%S")  # Time format
+    print("get_utc5_time Result:", final_time)  # Print the get_utc5_time result
+    return final_time
 
 if __name__ == "__main__":
     result = get_utc5_time()
-    #print("sum_parts Result:", result)  # Print the sum_parts result
     print(result)  # Print the time to capture it in the workflow
