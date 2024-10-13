f2ca476adde00c49058499834577c1f5b72f9ed4
JorgeCardona <jorgecardona@utp.edu.co>
2024-10-13 01:30:46 +0000

    fix comments

--- src/get_utc5_time.py
+++ src/get_utc5_time.py
@@ -1,9 +1,9 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 def get_utc5_time():
     # Get the current time in UTC-5
     utc_offset = timedelta(hours=-5)
-    current_time = datetime.utcnow() + utc_offset
+    current_time = datetime.now(timezone.utc) + utc_offset
     return current_time.strftime("%Y-%m-%d %H:%M:%S")  # Time format
 
 if __name__ == "__main__":
